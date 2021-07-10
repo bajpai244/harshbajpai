22af1e4b5cc823d1f33230118a4a469482b38daf
bajpai244 <bajpaiharsh244@gmail.com>
2021-07-11 02:05:00 +0530

    fix(favicon): fix case prolem favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         {/**Fonts import*/}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         {/**Ubuntu*/}
         <link
           href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
